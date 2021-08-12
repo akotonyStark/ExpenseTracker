2bb96a95c746fb8cb56a076a88336f9e514bbed6
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-08-12 10:47:54 +0000

    updated

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import './NewExpense.css'
 
-const NewExpense = () => {}
+const NewExpense = () => {
+  return (
+    <div className='new-expense'>
+      <form></form>
+    </div>
+  )
+}
 
 export default NewExpense
